e9a09892fbd1f647a2a914c037c86324b7460c37
Gonzalo Duran <gon.duran@duocuc.cl>
2024-10-28 17:43:46 -0300

    Entrega semana 2 fullstack 3 sumativa 1

--- tienda-ms-productos/src/test/java/com/example/tienda_ms_productos/controller/ProductoControllerTest.java
+++ tienda-ms-productos/src/test/java/com/example/tienda_ms_productos/controller/ProductoControllerTest.java
@@ -1,15 +1,10 @@
 package com.example.tienda_ms_productos.controller;
 
-import com.example.tienda_ms_productos.controller.ProductoController;
-import com.example.tienda_ms_productos.exception.BadRequestException;
 import com.example.tienda_ms_productos.exception.NotFoundException;
 import com.example.tienda_ms_productos.model.Producto;
 import com.example.tienda_ms_productos.service.ProductoService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
