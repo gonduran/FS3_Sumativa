725cab272754564f3f573c32b82a6fa5e5680564
Gonzalo Durán <gon.duran@duocuc.cl>
2024-12-11 23:11:00 -0300

    Entrega semana 9 fullstack 3 EFT

--- backend-arquetipo/tienda-ms-pedidos/src/main/java/com/example/tienda_ms_pedidos/controller/DetalleOrdenController.java
+++ backend-arquetipo/tienda-ms-pedidos/src/main/java/com/example/tienda_ms_pedidos/controller/DetalleOrdenController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/pedidos/detalles")
+@CrossOrigin(origins = "*")
 public class DetalleOrdenController {
 
     private static final Logger log = LoggerFactory.getLogger(DetalleOrdenController.class);
--- tienda-front/src/environments/environment.ts
+++ tienda-front/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUsuarios: 'http://localhost:8092/api/usuarios',
-  apiProductos: 'http://localhost:8093/api',
-  apiPedidos: 'http://localhost:8094/api',
+  apiUsuarios: 'http://ip172-18-0-18-ctd38a2im2rg00dasffg-8092.direct.labs.play-with-docker.com/api/usuarios',
+  apiProductos: 'http://ip172-18-0-20-ctd38a2im2rg00dasffg-8093.direct.labs.play-with-docker.com/api',
+  apiPedidos: 'http://ip172-18-0-17-ctd38a2im2rg00dasffg-8094.direct.labs.play-with-docker.com/api',
 };
